02fb02c51f6864f856bd50fdd4030f9fddf6ffdd
kidoplex <maros7929@gmail.com>
2024-10-07 10:26:25 +0200

    init

---
+++ src/app/auth/odhlasenie/page.tsx
@@ -0,0 +1,14 @@
+// src/app/auth/odhlasenie/page.tsx
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: "Odhlásenie užívateľa | ZoškaSnap" };
+
+export default function SignOut() {
+
+  return (
+
+      <Typography> Odhlásenie užívateľa </Typography>
+
+  );
+}
