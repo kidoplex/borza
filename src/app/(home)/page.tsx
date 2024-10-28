e4ba5e13e07881a2df84fb9a59ed39f441469ae8
kidoplex <maros7929@gmail.com>
2024-10-28 10:40:43 +0100

    google

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,36 +1,24 @@
+
 // src/app/page.tsx
 
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { useSession } from "next-auth/react";
-import AuthHomeView from "@/sections/AuthHomeView"; // Komponent pre prihlásených používateľov
-import NonAuthHomeView from "@/sections/NonAuthHomeView"; // Komponent pre neprihlásených používateľov
-import { Box, CircularProgress } from "@mui/material";
-
-export const metadata = { title: "Domov | ZoškaSnap" };
-
-export default function Home() {
-  <Container> 
-      <Typography> Domovská stránka </Typography>
-    </Container>
-  const { data: session, status } = useSession(); // Používanie useSession na získanie informácií o používateľovi
-  if (status === 'loading') {
-    return (
-      <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-        <CircularProgress />
-      </Box>
-    );
-  }
-
-  // Ak používateľ nie je prihlásený, zobraz NonAuthHomeView
-  if (!session) {
-    return <NonAuthHomeView />;
-  }
-
-  // Ak používateľ je prihlásený, zobraz AuthHomeView
-  return <AuthHomeView />;
 
 
+//src/app/api/auth/[...nextauth]/authOptions.ts
+//import * as React from 'react';
+//import { getServerSession } from 'next-auth/react';
+import { authOptions } from  "../api/auth/[...nextauth]/authOptions";
+import AuthHomeView from '../../sections/AuthHomeView'; // Adjust the path as necessary
+import NonAuthHomeView from '../../sections/NonAuthHomeView'; // Adjust the path as necessary
+import { getServerSession } from 'next-auth';
+
+export const metadata = { title: 'Domov | ZoskaSnap' };
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
+    
+  
 }
 
 
@@ -52,43 +40,4 @@ export default function Home() {
 
 
 
-{/* 
 
-
-// import HomeIcon from "@mui/icons-material/Home";
-// import SearchIcon from "@mui/icons-material/Search";
-// import AddCircleIcon from "@mui/icons-material/AddCircle";
-// import NotificationsIcon from "@mui/icons-material/Notifications";
-// import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-// import { useState } from "react";
-
-//   const [navValue, setNavValue] = useState(0);
-    // <Container maxWidth="sm" sx={{ minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "space-between" }}>
-    //   <Box component="main" sx={{ flexGrow: 1, paddingTop: 2 }}>
-    //     <Typography variant="h4" gutterBottom>
-    //       Vitajte na SnapZoška!
-    //     </Typography>
-    //     <Typography variant="body1">
-    //       Toto je vaša úvodná stránka. Tu nájdete príspevky od používateľov, pridáte vlastné alebo preskúmate nové príspevky.
-    //     </Typography>
-    //   </Box>
-
-    //   {/* Bottom Navigation */}
-    //   <BottomNavigation
-    //     value={navValue}
-    //     onChange={(event, newValue) => setNavValue(newValue)}
-    //     showLabels
-    //   >
-    //     <BottomNavigationAction label="Domov" icon={<HomeIcon />} />
-    //     <BottomNavigationAction label="Hľadať" icon={<SearchIcon />} />
-    //     <BottomNavigationAction label="Pridať" icon={<AddCircleIcon />} />
-    //     <BottomNavigationAction label="Notifikácie" icon={<NotificationsIcon />} />
-    //     <BottomNavigationAction label="Profil" icon={<AccountCircleIcon />} />
-    //   </BottomNavigation>
-    // </Container> */}
-
-
-
-
-
-    
