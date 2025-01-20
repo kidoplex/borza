14deaae1939d3352a8db7eef5be25519aaf11603
kidoplex <maros7929@gmail.com>
2025-01-20 13:02:39 +0100

    working

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { IconButton, useTheme } from "@mui/material";
+import { IconButton } from "@mui/material";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { useThemeContext } from "@/components/ThemeProviderWrapper";
